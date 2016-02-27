9af044c92307af7c79528ba5d3f25d8f92e8be39
Julia <yuliya96a@yandex.ua>
2016-02-27 03:02:10 +0200

    Create Main.java

---
+++ HomeWork1/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Lesson lessons1 = new Lesson();
+		
+		    lessons1.setType ("Practical");
+        lessons1.setCourse("2");
+        lessons1.setGroup("Economic Cybernetics");
+        lessons1.setTeacher("Fedorov N.");
+        lessons1.setAudience(407);
+        lessons1.setTime(9.20);
+
+        System.out.println(lessons1.read());
+    }
+}
