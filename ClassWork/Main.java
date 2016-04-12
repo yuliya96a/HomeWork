1b4e67a3f2d0ae6e97ae8084df14806c3c3cf500
Julia <yuliya96a@yandex.ua>
2016-04-12 21:06:45 +0300

    Create Main.java

---
+++ ClassWork/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Exam exam1 = new Exam();
+
+        exam1.setSubject ("Programming");
+        exam1.getSubject("Programming");
+        exam1.setMark("4");
+        exam1.getMark("4");
+
+    }
+}
