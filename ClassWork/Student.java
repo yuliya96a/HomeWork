2031484a83022e603f762920f39b883325cf69ee
Julia <yuliya96a@yandex.ua>
2016-04-12 21:15:27 +0300

    Create Student.java

---
+++ ClassWork/Student.java
@@ -0,0 +1,43 @@
+import com.company.Exam;
+
+public class Student {
+
+    protected String subject;
+    protected int mark;
+    protected Exam[] examArray;
+    protected int number;
+
+    public Student() {
+        this.examArray = new Exam[10];
+        this.number = 0;
+    }
+
+    public String getSubject() {
+        return subject;
+
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+    public int getMark() {
+        return mark;
+
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+    public void addExam(Exam exam) {
+        this.examArray[this.number] = exam;
+        this.number++;
+    }
+    public String toString() {
+        String result = this.subject + " ready to pay salary for " + this.mark + " employees." + "\n--------\n";
+        for(int i = 0; i < this.number; i++) {
+            result += this.examArray[i].toString();
+        }
+
+        return result;
+    }
+}
