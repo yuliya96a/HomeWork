f507789f9d695be66779ce00081a468fe731d982
Julia <yuliya96a@yandex.ua>
2016-04-13 09:15:25 +0300

    Update Lesson.java

--- HomeWork1/Lesson.java
+++ HomeWork1/Lesson.java
@@ -1,34 +1,61 @@
+package com.company;
+
 public class Lesson {
-	
-	protected int course;
-	protected String group;
-	protected String teacher;
-	protected int audience;
-	protected float time;
+
+    protected int course;
+    protected String group;
+    protected String teacher;
+    protected int audience;
+    protected double time;
 
 
-  public void setCourse(String course) {
-    this.course = course;
-  }
+    public void setCourse(int course) {
+        this.course = course;
+    }
+    public int getCourse() {
+        return course;
+    }
 
-  public void setGroup(String group) {
-    this.group = group;
-  }
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    public String getGroup() {
+        return group;
+    }
 
-  public void setTeacher(String teacher) {
-    this.teacher = teacher;
-  }
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+    public String getTeacher() {
+        return teacher;
+    }
 
-  public void setAudience (int audience) {
-    this.audience = audience;
-  }
+    public void setAudience(int audience) {
+        this.audience = audience;
+    }
+    public int getAudience(){
+        return audience;
+    }
 
-  public void setTime (float time) {
-    this.time = time;
-  }
+    public void setTime (double time) {
+        this.time = time;
+    }
+    public double getTime () {
+        return time;
+    }
 
-   public String read() {
-      String result = "About Lesson "\nCourse: " + this.course + "\nGroup: " + this.group + "\nTeacher: " + this.teacher + "\nAudience: " + this.audience + "\nTime: " + this.time;
-      return result;
-   }
+    public Lesson () {}
+
+    public Lesson (int course, String group ,String teacher, int audience, double time) {
+        this.course = course;
+        this.group = group;
+        this.teacher = teacher;
+        this.audience = audience;
+        this.time = time;
+
+    }
+    public String read() {
+        String result = "\nCourse: " + this.course + "\nGroup: " + this.group + "\nTeacher: " + this.teacher + "\nAudience: " + this.audience + "\nTime: " + this.time;
+        return result;
+    }
 }
