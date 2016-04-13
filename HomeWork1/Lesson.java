7d69320216f4eb8f600f1cea57888386f9f3089d
Julia <yuliya96a@yandex.ua>
2016-04-13 09:18:08 +0300

    Update Lesson.java

--- HomeWork1/Lesson.java
+++ HomeWork1/Lesson.java
@@ -4,7 +4,7 @@ public class Lesson {
 
     protected int course;
     protected String group;
-    protected String teacher;
+    protected String teachername;
     protected int audience;
     protected double time;
 
@@ -23,11 +23,11 @@ public class Lesson {
         return group;
     }
 
-    public void setTeacher(String teacher) {
-        this.teacher = teacher;
+    public void setTeachername(String teachername) {
+        this.teachername = teachername;
     }
     public String getTeacher() {
-        return teacher;
+        return teachername;
     }
 
     public void setAudience(int audience) {
@@ -49,13 +49,16 @@ public class Lesson {
     public Lesson (int course, String group ,String teacher, int audience, double time) {
         this.course = course;
         this.group = group;
-        this.teacher = teacher;
+        this.teachername = teacher;
         this.audience = audience;
         this.time = time;
 
     }
     public String read() {
-        String result = "\nCourse: " + this.course + "\nGroup: " + this.group + "\nTeacher: " + this.teacher + "\nAudience: " + this.audience + "\nTime: " + this.time;
+        String result = "\nCourse: " + this.course + "\nGroup: " + this.group + "\nTeacher: " + this.teachername + "\nAudience: " + this.audience + "\nTime: " + this.time;
         return result;
     }
 }
+
+
+
