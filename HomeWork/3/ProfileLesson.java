d225df2cee8fc1c410f6c4d206a03bba30a42904
Julia <yuliya96a@yandex.ua>
2016-05-08 15:53:45 +0300

    Create ProfileLesson.java

---
+++ HomeWork/3/ProfileLesson.java
@@ -0,0 +1,29 @@
+package com.company;
+
+
+public class ProfileLesson extends Lesson {
+    protected int numberOfLabs;
+
+    public ProfileLesson(int numberOfLabs) {
+        this.numberOfLabs = numberOfLabs;
+    }
+
+
+    public ProfileLesson(String coursename, String teacher, int audience, double time, int numberOfLabs) {
+        super(coursename, teacher, audience, time);
+        this.numberOfLabs = numberOfLabs;
+    }
+
+    public int getNumberOfLabs() {
+        return numberOfLabs;
+    }
+
+    public void setNumberOfLabs(int numberOfLabs) {
+        this.numberOfLabs = numberOfLabs;
+    }
+
+    @Override
+    public String read() {
+        return "\nCoursename: " + this.coursename +  "\nTeacher: " + this.teachername + "\nAudience: " + this.audience + "\nTime: " + this.time + "\nNumber of Labs" + this.numberOfLabs;
+    }
+}
