c5bfa55e5b10e76fb7085aeec15172fc33297c7a
Julia <yuliya96a@yandex.ua>
2016-05-08 13:28:28 +0300

    Update Main.java

--- HomeWork/Main.java
+++ HomeWork/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Lesson lessons1 = new Lesson();
 
-        lessons1.setCourse(2);
-        lessons1.setGroup("Economic Cybernetics");
+        lessons1.setCoursename("Economic Cybernetics");
         lessons1.setTeachername("Fedorov N.");
         lessons1.setAudience(407);
         lessons1.setTime(9.20);
